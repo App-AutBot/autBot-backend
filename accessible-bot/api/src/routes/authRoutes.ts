209923e37bca6063d604fd45a89f7f62f93b3984
Isabella Costa <isabellacosta.js@gmail.com>
2025-11-28 14:39:01 -0300

    Limpando a main

--- accessible-bot/api/src/routes/authRoutes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { AuthController } from "../controllers/authController";
-
-const authRouter = Router();
-
-authRouter.post("/forgot-password", AuthController.forgotPassword);
-authRouter.post("/reset-password/:token", AuthController.resetPassword);
-
-export default authRouter;
